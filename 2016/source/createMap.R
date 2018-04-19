a7dca0f6f9d3d343565dd1a0184384d11cbdde3c
Christopher Prener <chris.prener@slu.edu>
2018-04-18 20:38:56 -0500

    add 2016 maps

---
+++ 2016/source/createMap.R
@@ -0,0 +1,79 @@
+# create map of race by county
+
+## Dependencies
+### tidyverse
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+### mapping
+library(classInt)
+library(sf)
+library(tigris)
+
+### other
+library(prener)
+
+## Create state outline
+### download missouri
+mo <- states(cb = FALSE, resolution = "20m")
+
+### convert to sf object
+mo <- st_as_sf(mo)
+
+### subset observations
+mo <- filter(mo, STATEFP == 29)
+
+### download missouri counties
+moCounties <- counties(state = "MO", cb = FALSE, resolution = "20m")
+
+### convert to sf object
+moCounties <- st_as_sf(moCounties)
+
+### subset columns
+moCounties <- select(moCounties, GEOID, COUNTYFP, NAMELSAD, ALAND)
+
+### read in tabular data
+birthData <- read_csv("2016/data/MO_HEALTH_lowBirth.csv")
+
+### clean up data
+birthData %>%
+  rename(lowBirths = `Number of Low Births`) %>%
+  rename(liveBirths = `Number of Live Births`) %>%
+  mutate(pctLow = (lowBirths/liveBirths)*100) %>%
+  mutate(FIPS = as.character(FIPS)) %>%
+  select(FIPS, pctLow) -> lowBirth
+  
+### combine spatial and geometric data
+birthMap <- left_join(moCounties, lowBirth, by = c("GEOID" = "FIPS"))
+
+
+jenks <- classIntervals(birthMap$pctLow, n=5, style="jenks")
+lbw <- cut(birthMap$pctLow, breaks = c(jenks$brks))
+
+## base map
+base <- ggplot() + 
+  geom_sf(data = mo, fill = "#ffffff", color = NA) + 
+  geom_sf(data = birthMap, mapping = aes(fill = lbw), color = NA) +
+  geom_sf(data = mo, fill = NA, color = "#000000", size = .25) +
+  scale_fill_brewer(palette = "RdPu", name = "Percent",
+                    labels = c("3.83 - 6.68", "6.69 - 7.80", "7.81 - 8.84", "8.85 - 10.60", "10.61 - 13.70")) +
+  labs(
+    title = "Low Birth Weight Births by County, 2016",
+    subtitle = "Live Births in Missouri with Weight Under 5 lbs., 8 oz.",
+    caption = "Data via County Health Rankings \nMap by Christopher Prener, Ph.D."
+  ) 
+
+cp_plotSave(filename = "2016/results/lbwMap16-base.png", plot = base, preset = "lg", dpi = 500)
+
+## map with white background
+map01 <- base +
+  cp_sequoiaTheme(background = "white", map = TRUE)
+
+cp_plotSave(filename = "2016/results/lbwMap16-white.png", plot = map01, preset = "lg", dpi = 500)
+
+## map with transparent background
+map02 <- base +
+  cp_sequoiaTheme(background = "transparent", map = TRUE)
+
+cp_plotSave(filename = "2016/results/lbwMap16-trans.png", plot = map02, preset = "lg", dpi = 500)
